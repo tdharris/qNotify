66f9b66aa574e252af7907308a2cc4de617e3f74
tdharris <tylerdavidharris@gmail.com>
2014-07-22 16:15:28 -0600

    working on logrotate

--- forever-start.js
+++ forever-start.js
@@ -5,6 +5,24 @@ var forever = require('forever-monitor'),
     config = require('./config.json');
 
 // Check for logrotate
+function ensureExists(path, mask, cb) {
+    if (typeof mask == 'function') { // allow the `mask` parameter to be optional
+        cb = mask;
+        mask = 0777;
+    }
+    fs.mkdir(path, mask, function(err) {
+        if (err) {
+            if (err.code == 'EEXIST') cb(null); // ignore the error if the folder already exists
+            else cb(err); // something else went wrong
+        } else cb(null); // successfully created folder
+    });
+}
+
+var logDirectory = path.resolve(__dirname, config.logDirectory);
+ensureExists(logDirectory, 0744, function(err) {
+    if (err) logme.error('Problem creating log directory: ', logDirectory);
+});
+
 var logRotateFile = path.resolve(__dirname, config.logDirectory, '*.log') + " { \n \
     compress \n \
     compresscmd /usr/bin/gzip \n \
@@ -17,12 +35,25 @@ var logRotateFile = path.resolve(__dirname, config.logDirectory, '*.log') + " { 
     create 640 root root \n \
 }";
 
-fs.writeFile('/etc/logrotate.d/qNotify', logRotateFile, function(err) {
-    if (err) {
-        logme.error('Problem creating logrotate file: /etc/logrotate.d/qNotify: ', err);
-    } else {
-        logme.info('Created /etc/logrotate.d/qNotify');
+var createLogRotate = function() {
+    fs.writeFile('/etc/logrotate.d/qNotify', logRotateFile, function(err) {
+        if (err) {
+            logme.error('Problem creating logrotate file: /etc/logrotate.d/qNotify: ', err);
+        } else {
+            logme.info('Configured /etc/logrotate.d/qNotify');
+        }
+    });
+}
+
+fs.readFile('/etc/logrotate.d/qNotify', 'utf8', function(err,data){
+    if (err) createLogRotate();
+    else {
+        if (data.indexOf(logDirectory) == -1) {
+            // Log directory change, reconfigure logrotate file:
+            createLogRotate();
+        }
     }
+
 });
 
 var child = new(forever.Monitor)('app.js', {
